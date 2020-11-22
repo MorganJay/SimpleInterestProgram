74d6075901c444152a67f38a65d6677d29688f49
James Morgan O <MorganJay@users.noreply.github.com>
2020-11-22 20:24:08 +0100

    commit

--- CompoundInterestApp/Program.cs
+++ CompoundInterestApp/Program.cs
@@ -6,58 +6,72 @@ namespace CompoundInterestApp
 {
     internal class Program
     {
-        // A = principal * (1 + rate/n)^ number of times interest is compounded in time period * time in year/months
+        private const decimal rate = 0.0625m;
+        private const uint quarterCompound = 4;
+        private const uint annualCompound = 1;
+        private const uint weeklyCompund = 52;
+        private const uint dailyCompound = 365;
+
         private static void Main(string[] args)
         {
-            const decimal rate = 0.0625m;
-            const uint quarterCompound = 4;
-            const uint annualCompound = 1;
-            const uint weeklyCompund = 52;
-            const uint dailyCompound = 365;
+            WelcomeUser();
+            CalculateInterest();
+            Closing();
+        }
+
+        private static void WelcomeUser()
+        {
             Console.WriteLine("*******Welcome to National Bank********");
             Console.WriteLine("Now, you can know how much you would receive if you invest with us after any VALID period of time you wish");
             Console.WriteLine("Note that we deal in compounded interest only....which is the best kind of interest");
             Console.WriteLine($"Our interest rate is {rate:P}");
             Thread.Sleep(1000);
+        }
 
+        private static void CalculateInterest()
+        {
             Console.WriteLine("Please enter the principal amount you wish to invest..");
-            decimal principal = Decimal.Parse(Console.ReadLine());
+            decimal principal = decimal.Parse(Console.ReadLine());
 
             Console.WriteLine("Should the interest be given annually, quarterly, weekly or daily ? ");
             var compoundRate = Console.ReadLine().ToLower();
 
             Console.WriteLine("Please enter the time period in months...");
-            decimal duration = Decimal.Parse(Console.ReadLine());
+            decimal duration = decimal.Parse(Console.ReadLine());
 
-            decimal value;
+            decimal finalAmount;
             switch (compoundRate)
             {
                 case "annually":
-                    value = principal * (decimal)Math.Pow((double)(1 + (rate / annualCompound)), (double)(annualCompound * duration / 12));
-                    Console.WriteLine($"The total amount you will receive at the end of {duration} month(s) is {value:C}");
+                    finalAmount = principal * (decimal)Math.Pow((double)(1 + (rate / annualCompound)), (double)(annualCompound * duration / 12));
+                    Console.WriteLine($"The total amount you will receive at the end of {duration} month(s) is {finalAmount:C}");
                     break;
 
                 case "quarterly":
-                    value = principal * (decimal)Math.Pow((double)(1 + (rate / quarterCompound)), (double)(quarterCompound * duration / 12));
-                    Console.WriteLine($"The total amount you will receive at the end of {duration} month(s) is {value:C}");
+                    finalAmount = principal * (decimal)Math.Pow((double)(1 + (rate / quarterCompound)), (double)(quarterCompound * duration / 12));
+                    Console.WriteLine($"The total amount you will receive at the end of {duration} month(s) is {finalAmount:C}");
                     break;
 
                 case "weekly":
-                    value = principal * (decimal)Math.Pow((double)(1 + (rate / weeklyCompund)), (double)(weeklyCompund * duration / 12));
-                    Console.WriteLine($"Wow!! You will receive {value:C} after {duration} months");
+                    finalAmount = principal * (decimal)Math.Pow((double)(1 + (rate / weeklyCompund)), (double)(weeklyCompund * duration / 12));
+                    Console.WriteLine($"Wow!! You will receive {finalAmount:C} after {duration} month(s)");
                     break;
 
                 case "daily":
-                    value = principal * (decimal)Math.Pow((double)(1 + (rate / dailyCompound)), (double)(dailyCompound * duration / 12));
-                    Console.WriteLine($"OMO Lord of Lavish!! After {duration} months you will cash out {value:C}");
+                    finalAmount = principal * (decimal)Math.Pow((double)(1 + (rate / dailyCompound)), (double)(dailyCompound * duration / 12));
+                    Console.WriteLine($"OMO Lord of Lavish!! After {duration} month(s) you will cash out {finalAmount:C}");
                     break;
 
                 default:
                     Console.WriteLine("Invalid input");
                     break;
             }
+        }
+
+        private static void Closing()
+        {
             Console.WriteLine("Thank you for banking with us :)");
-            Thread.Sleep(5000);
+            Thread.Sleep(10000);
         }
     }
 }
