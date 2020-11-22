be78bfb3d72df316a9ba341e5275328d23f176fc
James Morgan O <MorganJay@users.noreply.github.com>
2020-11-22 02:18:32 +0100

    Done for the night

--- SimpleInterestProgram/Program.cs
+++ SimpleInterestProgram/Program.cs
@@ -2,10 +2,10 @@
 using System.Globalization;
 using System.Collections.Generic;
 
-// You are employed as a junior dev at National bank and you're asked to create a small program to allow prospective customers know the sum 
+// You are employed as a junior dev at National bank and you're asked to create a small program to allow prospective customers know the sum
 //they would collect after saving in the bank for a period of time.
-// The interest rate is fixed at 6.25% per month by the federal reserve, using simple interest. 
-//Create a simple console program that would allow the user speculate an amount, 
+// The interest rate is fixed at 6.25% per month by the federal reserve, using simple interest.
+//Create a simple console program that would allow the user speculate an amount,
 //and returns to the user the total amount he would get based on the number of months he specifies.
 
 //Do the one for compound interest
@@ -16,27 +16,15 @@ namespace SimpleInterestProgram
     {
         private static void Main(string[] args)
         {
-            var amounts = new List<decimal>();
-            decimal total = 0;
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write("Enter a number");
-                var number = decimal.Parse(Console.ReadLine());
-                amounts.Add(number);
-            }
-
-            foreach (var number in amounts)
-            {
-                total += number;
-            }
-
-            Console.WriteLine("The total sum of the numbers you entered is {0}", total);
-            Console.ReadLine();
+            SimpleCase();
+            //IfCase()
+            //SwitchCase()
+            //ForEach()
         }
 
-        public static void SimpleCase()
+        private static void SimpleCase()
         {
-            const decimal rate = 6.25m;
+            const decimal rate = 5.5m;
             Console.WriteLine("*******Welcome to National Bank********");
 
             Console.WriteLine("Please enter your Principal amount..");
@@ -45,14 +33,14 @@ namespace SimpleInterestProgram
             Console.WriteLine("Please enter your tenure in months...");
             uint duration = (uint)int.Parse(Console.ReadLine());
 
-            var interest = principal * rate * duration / 100;
+            var interest = principal * rate * duration / 12 / 100;
             var totalSum = interest + principal;
-            Console.WriteLine($"The total amount you will receive at the end of {duration} month(s) is {totalSum}.");
+            Console.WriteLine($"The total amount you will receive at the end of {duration} month(s) is {totalSum:C}");
             Console.WriteLine("Thank you for banking with us :)");
             Console.ReadLine();
         }
 
-        public static void IfCase()
+        private static void IfCase()
         {
             const decimal rate = 6.25m;
             const decimal vat = 7.5m;
@@ -80,7 +68,7 @@ namespace SimpleInterestProgram
             Console.ReadLine();
         }
 
-        public static void SwitchCase()
+        private static void SwitchCase()
         {
             const decimal rate = 6.25m;
             const decimal vat = 7.5m;
@@ -128,6 +116,25 @@ namespace SimpleInterestProgram
             var totalSum = interest + principal;
             Console.WriteLine("The total amount you will receive at the end of {0} month(s) is {1:C3}.", duration, totalSum);
             Console.WriteLine("Thank you for banking with us :)");
+            Console.ReadLine();
+        }
+
+        private static void ForEach()
+        {
+            var amounts = new List<decimal>();
+            decimal total = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter a number");
+                decimal number = decimal.Parse(Console.ReadLine());
+                amounts.Add(number);
+            }
+
+            foreach (var number in amounts)
+            {
+                total += number;
+            }
+            Console.WriteLine("The total sum of the numbers you entered is {0}", total);
             Console.ReadLine();
         }
     }
